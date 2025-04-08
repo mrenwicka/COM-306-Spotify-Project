0cac0dfa36ccae493a9ad63f4c43295a26faa94c
gi2425 <115180759+gi2425@users.noreply.github.com>
2025-04-08 15:39:25 -0400

    Update spotifyinfo.py

--- spotifyinfo.py
+++ spotifyinfo.py
@@ -1,5 +1,5 @@
 import os
-import time
+
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy
 from dotenv import load_dotenv
@@ -16,8 +16,11 @@ class spotify:
             client_id=self.client_id,
             client_secret=self.client_secret,
             redirect_uri=self.redirect_uri,
-            scope=["user-top-read"
-                  "user-read-playback-state",
-                "user-read-currently-playing"]
+            scope=["user-top-read",
+       "user-read-playback-state",
+       "user-read-currently-playing"]
         ))
+
+    def get_client(self):
+        return self.sp
     
