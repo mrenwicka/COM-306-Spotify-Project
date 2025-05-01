0b79c4733372106d26ce522f72f084aec9e3c15f
Echo-Sunshine <112265759+Echo-Sunshine@users.noreply.github.com>
2025-05-01 15:32:28 -0400

    login issue
    talk to me/give me your email for your spotify accounts I think I've figured it out

---
+++ userinfo.py
@@ -0,0 +1,27 @@
+from spotifyinfo import spotify
+
+class userinfo():
+    def __init__(self):
+        self.sp = spotify().get_client()
+    
+    def get_user_id(self):
+        user_profile = self.sp.current_user()
+        return user_profile['id']
+
+    def get_username(self, id):
+        info = self.sp.user(id)
+        return info['display_name']
+    
+    def get_profile_pic(self, id):
+        info = self.sp.user(id)
+        profile_pic = info.get('images')
+        try:
+            return profile_pic[0]['url']
+        except:
+            return None
+
+if __name__ == "__main__": 
+    user_info = userinfo()
+    user_id = user_info.get_user_id()
+    print(user_info.get_username(user_id))
+    print(user_info.get_profile_pic(user_id))
