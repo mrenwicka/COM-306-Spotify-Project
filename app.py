be68f0bebc2c57aa66b1894a6d83c0e61c0d1e7f
Ahren <ahrensalazar3120@gmail.com>
2025-05-05 19:33:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Redirect to the login page
+    # Redirect to the login pageç
     return redirect(url_for('weather'))
 
 @app.route('/stats')
@@ -42,21 +42,21 @@ def stats():
                            profile_pic=profile_pic,
                            genre_chart_url=genre_chart_path,
                            artist_chart_url=artist_chart_path,
-                           playlist_1=playlist_1,
-                           playlist_2=playlist_2)
+                           playlist_1 = playlist_1,
+                           playlist_2 = playlist_2)
     
 
 @app.route('/home')
 def weather():
     weather = weatherplaylists()
-    spotify = SpotifyTrackInfo()
+    spotify_info = SpotifyTrackInfo()
 
     user_client = userinfo()
     user_id = user_client.get_user_id()
     username = user_client.get_username(user_id)
     profile_pic = user_client.get_profile_pic(user_id)
 
-    song_info = spotify.get_current_song_info()
+    song_info = spotify_info.get_current_song_info()
     if song_info == "same":
         song_info = None
 
@@ -67,15 +67,23 @@ def weather():
     month_client = monthlyplaylist()
     month = month_client.current_month()
     monthly_playlist_url = month_client.monthly_playlist(month)
-    
-    
+
+
+    sp_client = spotify().get_client()
+    top_songs_client = SpotifyTopSongs(sp_client, limit=3)
+
+    personal_playlists = top_songs_client.your_playlists(max_count=2)
+    playlist_1 = personal_playlists[0] if len(personal_playlists) > 0 else None
+    playlist_2 = personal_playlists[1] if len(personal_playlists) > 1 else None
 
     return render_template('index.html',
                            weather_playlist_url=weather_playlist_url,
                            song_info=song_info,
                            monthly_playlist_url=monthly_playlist_url,
                            username=username,
-                           profile_pic=profile_pic,)
+                           profile_pic=profile_pic,
+                            playlist_1 = playlist_1,
+                           playlist_2 = playlist_2)
 
 
 
